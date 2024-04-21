748e2832262418a9ec735d6350d4a912a1fddd3d
vanno23 <mmmartsy@gmail.com>
2024-04-21 17:55:49 +0400

    main

---
+++ assets/js/contact.js
@@ -0,0 +1,36 @@
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_5z2hzob",
+      "template_02t4388",
+      "#contact-form",
+      "ePAzXmb2e_kThK0BN"
+    )
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
+
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
