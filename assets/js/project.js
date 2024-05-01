0204d46bf65e425cf8303eb16fbc6b9bf15a7646
vanno23 <118561252+vanno23@users.noreply.github.com>
2024-05-01 17:44:04 +0400

    main

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,47 +1,164 @@
 const projects = [
   {
     id: 1,
-    image: "assets/img/projects/Colour.jpg",
+    images: ["./assets/img/projects/Colour.jpg"],
     title: "Residential Apartment",
     subtitle: "Interior Design",
     link: "project.html?id=1",
     details:
-      "Within the confines of this studio apartment, you'll find two bedrooms, two bathrooms, a living room with an adjoining shared kitchen, a music studio, and a stunning terrace. Each space is adorned with a palette inspired by the lively and vibrant hues of summer. Upon observing the color schemes of the rooms, one can easily discern the essence of the summer season infused into each carefully chosen shade. <br/> In the child's room, a serene ambiance is curated through a blend of blue tones, soft yellow hues, and accents of gentle red. Enhanced by warm lighting, the space exudes tranquility and comfort, creating a cozy haven for rest and play.",
+      "Within the confines of this studio apartment, you'll find two bedrooms, two bathrooms, a living room with an adjoining shared kitchen, a music studio, and a stunning terrace. Each space is adorned with a palette inspired by the lively and vibrant hues of summer. Upon observing the color schemes of the rooms, one can easily discern the essence of the summer season infused into each carefully chosen shade. <br /> <br /> In the child's room, a serene ambiance is curated through a blend of blue tones, soft yellow hues, and accents of gentle red. Enhanced by warm lighting, the space exudes tranquility and comfort, creating a cozy haven for rest and play.",
   },
   {
     id: 2,
-    image: "assets/img/work-2.png",
-    title: "Creative Website",
-    subtitle: "Website Design",
+    images: [
+      "./assets/img/projects/Meditation/meditation1.jpg",
+      "./assets/img/projects/Meditation/meditation2.jpg",
+    ],
+    title: "Meditation",
+    subtitle: "Interior Design",
     link: "project.html?id=2",
+    details:
+      "For my bachelor&#39;s degree project, I chose to design a meditation center nestled near the serene Kakhisi Lake in the breathtaking landscapes of Georgia. Drawing inspiration from the tranquil surroundings, my vision is to create a space that harmonizes with nature and fosters deep contemplation and inner peace. Through mindful architectural elements, sustainable materials, and an integration of the natural environment, the center will offer a serene sanctuary for individuals seeking spiritual solace and introspection amidst the beauty of Kakhisi Lake. <br /> <br /> Within this project, the focal point is a magnificent meditation hall, designed to inspire serenity and inner reflection. Adjacent to it, lies a yoga studio, where individuals can find balance and harmony through physical practice. Additionally, there&#39;s a luxurious spa and rejuvenating sauna, offering spaces for relaxation and holistic well-being.  <br /> <br /> Nestled amidst the lush landscape are healing pools, providing a tranquil setting for renewal and restoration. For moments of communal enjoyment, there&#39;s an outdoor cinema under the starry skies, where visitors can gather and connect with nature in a unique way. With these diverse amenities and more, the meditation center near Kakhisi Lake promises a holistic sanctuary for mind, body, and soul.",
+    downloadFiles: [
+      "./assets/Files/Meditation/meditation1.png",
+      "./assets/Files/Meditation/meditation2.png",
+    ],
   },
   {
     id: 3,
-    image: "assets/img/work-3.png",
-    title: "Creative Website",
-    subtitle: "Website Design",
+    images: [
+      "./assets/img/projects/PH/PH-1.jpg",
+      "./assets/img/projects/PH/PH-2.jpg",
+      "./assets/img/projects/PH/PH-3.jpg",
+    ],
+    title: "PH",
+    subtitle: "Interior Design",
     link: "project.html?id=3",
+    details:
+      "Integrating Physics, Shading, and Acoustics in Architectural Design",
+    downloadFiles: [
+      "./assets/Files/PH/PH-1.png",
+      "./assets/Files/PH/PH-2.png",
+      "./assets/Files/PH/PH-3.png",
+    ],
   },
   {
     id: 4,
-    image: "assets/img/work-4.png",
-    title: "Creative Website",
-    subtitle: "Website Design",
+    images: [
+      "./assets/img/projects/Furniture/Furniture1.jpg",
+      "./assets/img/projects/Furniture/Furniture2.jpg",
+    ],
+    title: "Furniture",
+    subtitle: "Interior Design",
     link: "project.html?id=4",
+    details:
+      "The task was to design furniture for any Georgian Bank and I choose Tera Bank, infusing vibrant hues reminiscent of berry pink, purple, and green. I believe, project radiates modernity and functionality, seamlessly integrating these bold colors while ensuring the essence of the bank remains intact. With each piece carefully crafted, harmonious fusion of innovation and Georgian charm is orchestrated, and a new standard in contemporary design is set.",
+    downloadFiles: [
+      "./assets/Files/Furniture/Furniture1.png",
+      "./assets/Files/Furniture/Furniture2.png",
+    ],
   },
   {
     id: 5,
-    image: "assets/img/work-5.png",
-    title: "Creative Website",
-    subtitle: "Website Design",
+    images: [
+      "./assets/img/projects/Cafe/Cafe1.jpg",
+      "./assets/img/projects/Cafe/Cafe2.jpg",
+      "./assets/img/projects/Cafe/Cafe3.jpg",
+    ],
+    title: "Cafe",
+    subtitle: "Interior Design",
     link: "project.html?id=5",
+    details:
+      "In the nascent stages of my design journey, this café interior project holds a special place as one of my earliest endeavors. It represents not only a creative exploration but also a foundational step in my evolution as a designer. <br /><br /> Conceptualizing the integration of an indoor yard within a café setting challenged me to think innovatively and push the boundaries of traditional design norms <br /><br /> In my university years, this café interior project marked a significant milestone in my design education. It provided a platform for me to apply theoretical knowledge to real-world scenarios and hone my practical skills in interior design. <br /><br /> As one of my first major projects, it taught me invaluable lessons in project management, time allocation, and teamwork. Through research, experimentation, and feedback, I gained a deeper understanding of the multifaceted nature of design and its impact on individuals and communities. <br /><br /> Reflecting on this project now, I am grateful for the opportunities it provided for growth and learning. It instilled in me a sense of confidence and enthusiasm to pursue my passion for interior design with zeal and dedication. Looking ahead, I carry the lessons learned from this project as I continue to embark on new design endeavors and contribute to shaping inspiring spaces for the future",
+    downloadFiles: [
+      "./assets/Files/Cafe/Cafe1.png",
+      "./assets/Files/Cafe/Cafe2.png",
+      "./assets/Files/Cafe/Cafe3.png",
+    ],
   },
   {
     id: 6,
-    image: "assets/img/work-6.png",
-    title: "Creative Website",
-    subtitle: "Website Design",
+    images: [
+      "./assets/img/projects/Winery/wine-1.jpg",
+      "./assets/img/projects/Winery/wine-2.jpg",
+      "./assets/img/projects/Winery/wine-3.jpg",
+      "./assets/img/projects/Winery/wine-A1.jpg",
+    ],
+    title: "Winery",
+    subtitle: "Interior Design",
     link: "project.html?id=6",
+    details:
+      "the task was to develop a comprehensive understanding of the principles and practices involved in optimizing spatial layouts for various purposes, such as residential, commercial, or institutional settings. <br /><br /> Through a systematic approach, the project delved into the intricacies of space analysis, functional programming, and circulation design. It emphasized the importance of considering human factors, building codes, and sustainability principles in the planning process. <br /><br /> Students were tasked with assessing real-world scenarios, identifying spatial requirements, and proposing innovative solutions to enhance the efficiency and usability of the designated spaces. Emphasis was placed on developing critical thinking skills, spatial awareness, and the ability to communicate design intentions effectively. <br /><br /> By engaging in hands-on exercises, collaborative discussions, and design critiques, participants gained practical experience in space planning methodologies and honed their problem-solving abilities. <br /><br /> Overall, the space planning project provided students with a solid foundation in the fundamentals of spatial design, equipping them with the knowledge and skills necessary to tackle complex design challenges and create environments that are both functional and aesthetically pleasing.",
+    downloadFiles: [
+      "./assets/Files/Winery/wine-1.png",
+      "./assets/Files/Winery/wine-2.png",
+      "./assets/Files/Winery/wine-3.png",
+      "./assets/Files/Winery/wine-A1.png",
+    ],
+  },
+  {
+    id: 7,
+    images: [
+      "./assets/img/projects/GraphicDesign/graphic1.jpg",
+      "./assets/img/projects/GraphicDesign/graphic2.jpg",
+      "./assets/img/projects/GraphicDesign/graphic3.jpg",
+      "./assets/img/projects/GraphicDesign/graphic4.jpg",
+      "./assets/img/projects/GraphicDesign/Wine_Bottle_with_Glass.jpg",
+      "./assets/img/projects/GraphicDesign/wine1.jpg",
+      "./assets/img/projects/GraphicDesign/wine2.jpg",
+      "./assets/img/projects/GraphicDesign/wine3.jpg",
+    ],
+    title: "Graphic Design Projects",
+    subtitle: "Graphic Design",
+    link: "project.html?id=7",
+    details:
+      "As an interior designer with a deep passion for graphic design, I&#39;ve embarked on personal projects and collaborated with small companies to explore and express my love for visual aesthetics. These endeavors have allowed me to merge my expertise in interior design with my enthusiasm for graphic design, creating a diverse portfolio of projects that reflect both my creative vision and dedication to delivering captivating design solutions",
+    downloadFiles: [
+      "./assets/Files/GraphicDesign/graphic1.png",
+      "./assets/Files/GraphicDesign/graphic2.jpg",
+      "./assets/Files/GraphicDesign/graphic3.jpg",
+      "./assets/Files/GraphicDesign/graphic4.jpg",
+      "./assets/Files/GraphicDesign/Wine_Bottle_with_Glass.png",
+      "./assets/Files/GraphicDesign/wine1.png",
+      "./assets/Files/GraphicDesign/wine2.png",
+      "./assets/Files/GraphicDesign/wine3.png",
+    ],
+  },
+  {
+    id: 8,
+    images: [
+      "./assets/img/projects/MooodBoards/moodboard1.jpg",
+      "./assets/img/projects/MooodBoards/moodboard2.jpg",
+      "./assets/img/projects/MooodBoards/moodboard3.jpg",
+      "./assets/img/projects/MooodBoards/moodboard4.jpg",
+      "./assets/img/projects/MooodBoards/moodboard5.jpg",
+      "./assets/img/projects/MooodBoards/moodboard6.jpg",
+      "./assets/img/projects/MooodBoards/moodboard7.jpg",
+      "./assets/img/projects/MooodBoards/moodboard8.jpg",
+      "./assets/img/projects/MooodBoards/moodboard9.jpg",
+      "./assets/img/projects/MooodBoards/moodboard10.jpg",
+      "./assets/img/projects/MooodBoards/moodboard11.jpg",
+      "./assets/img/projects/MooodBoards/moodboard12.jpg",
+    ],
+    title: "Mood Boards",
+    subtitle: "Mood Board",
+    link: "project.html?id=8",
+    details:
+      "I find great pleasure in curating interior moodboards and collages, a creative pursuit that brings me immense joy and a profound sense of tranquility Creating interior moodboards and collages is my cherished pursuit, bringing me profound joy and tranquility. Each meticulously curated board, whether for professional endeavors or personal projects, stands as a testament to my unwavering passion for design, serving as a visually captivating expression of my creativity and aesthetic sensibilities.",
+    downloadFiles: [
+      "./assets/Files/Moodboards/moodboard1.png",
+      "./assets/Files/Moodboards/moodboard2.png",
+      "./assets/Files/Moodboards/moodboard3.pdf",
+      "./assets/Files/Moodboards/moodboard4.pdf",
+      "./assets/Files/Moodboards/moodboard5.png",
+      "./assets/Files/Moodboards/moodboard6.jpg",
+      "./assets/Files/Moodboards/moodboard7.png",
+      "./assets/Files/Moodboards/moodboard8.png",
+      "./assets/Files/Moodboards/moodboard9.png",
+      "./assets/Files/Moodboards/moodboard10.png",
+      "./assets/Files/Moodboards/moodboard11.png",
+      "./assets/Files/Moodboards/moodboard12.png",
+    ],
   },
 ];
 
@@ -55,20 +172,165 @@ if (projectId) {
   appendSlides();
 }
 
+function downloadFiles(filesString, project_title) {
+  const files = filesString.split(",");
+  const zip = new JSZip();
+  const promises = [];
+
+  files.forEach((filePath, index) => {
+    const trimmedFilePath = filePath.trim().replace(/^"(.*)"$/, "$1");
+    const filename = trimmedFilePath.substring(
+      trimmedFilePath.lastIndexOf("/") + 1
+    );
+
+    // Fetch each file and add it to the zip
+    const promise = fetch(trimmedFilePath)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch ${trimmedFilePath}`);
+        }
+        return response.blob();
+      })
+      .then((blob) => {
+        zip.file(filename, blob);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    promises.push(promise);
+  });
+
+  // After all files are fetched and added to the zip, generate the zip file
+  Promise.all(promises).then(() => {
+    zip.generateAsync({ type: "blob" }).then(function (blob) {
+      const link = document.createElement("a");
+      link.style.display = "none";
+
+      link.setAttribute("download", `project_files_${project_title}.zip`);
+      link.href = URL.createObjectURL(blob);
+
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+    });
+  });
+}
+
 function displayProjectDetails(projectId) {
   const projectDetails = document.getElementById("project_page");
   const project = projects.find((project) => project.id === projectId);
 
   if (project) {
-    projectDetails.innerHTML = `
-    <div class="project_page_img">
-        <img src="${project.image}" alt="${project.title}">
-    </div>
-    <div>
+    if (project.images.length === 1) {
+      projectDetails.innerHTML = `
+      <div class="project_container">
+        <div class="project_page_img">
+            <img src="${project.images[0]}" alt="${project.title}">
+        </div>
+        <div>
+            <h2 class='projectPage_title'>${project.title}</h2>
+            <p class='projectPage_details'>${project.details}</p>
+            <button class="button download_project" onclick="window.open('./assets/Files/Colour.pdf')">Download Project</button>
+        </div>
+        </div>`;
+    } else if (project.images.length === 2) {
+      projectDetails.innerHTML = `
+      <div class="project_container_typeTwo">
+        <div>
+          <h2 class='projectPage_title'>${project.title}</h2>
+          <p class='projectPage_details'>${project.details}</p>
+        </div>
+        <div class="project_page_img project_images">
+        ${project.images
+          .map((image) => {
+            return `
+              <img src="${image}" alt="${project.title}">
+          `;
+          })
+          .join("")}
+        </div>
+        <button class="button download_project" onclick="downloadFiles('${
+          project.downloadFiles
+        }', '${project.title}')">Download Project</button>
+
+      </div>`;
+    } else if (project.images.length >= 3 && project.id !== 6) {
+      let swiperSlides = "";
+      project.images.forEach((image) => {
+        swiperSlides += `<div class="swiper-slide"><img class="swiper-image" src="${image}" alt="${project.title}"></div>`;
+      });
+      projectDetails.innerHTML = `
+      <div>
         <h2 class='projectPage_title'>${project.title}</h2>
         <p class='projectPage_details'>${project.details}</p>
-        <button class="button download_project" onclick="window.open('./assets/Files/Colour.pdf')">Download Project</button>
-    </div>`;
+      </div>
+      <div class="swiper-container-wrapper">
+          <div class="swiper-container-secondary">
+              <div class="swiper-wrapper">
+                  ${swiperSlides}
+              </div>
+              <div class="swiper-button-next"></div>
+              <div class="swiper-button-prev"></div>
+          </div>
+        </div>
+        <button class="button download_project image_length_three" onclick="downloadFiles('${project.downloadFiles}', '${project.title}')">Download Project</button>
+      `;
+
+      let swiper = new Swiper(".swiper-container-secondary", {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        loop: true,
+
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
+      window.addEventListener("resize", () => {
+        swiper.update();
+      });
+    } else if (project.id === 6) {
+      let swiperSlides = "";
+      project.images.slice(0, 3).forEach((image) => {
+        swiperSlides += `<div class="swiper-slide"><img class="swiper-image" src="${image}" alt="${project.title}"></div>`;
+      });
+
+      projectDetails.innerHTML = `
+      <div class="project_container">
+        <div class="project_page_img">
+          <img src="${project.images[3]}" alt="${project.title}">
+        </div>
+        <div>
+          <h2 class='projectPage_title'>${project.title}</h2>
+          <p class='projectPage_details'>${project.details}</p>
+        </div>
+      </div>
+      <div class="swiper-container-wrapper">
+          <div class="swiper-container-secondary">
+              <div class="swiper-wrapper">
+                  ${swiperSlides}
+              </div>
+              <div class="swiper-button-next"></div>
+              <div class="swiper-button-prev"></div>
+          </div>
+        </div>
+        <button class="button download_project" onclick="downloadFiles('${project.downloadFiles}', '${project.title}')">Download Project</button>
+      `;
+
+      let swiper = new Swiper(".swiper-container-secondary", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+    }
   } else {
     projectDetails.innerHTML = "<p>Project not found.</p>";
   }
@@ -85,7 +347,7 @@ function generateSlideHTML() {
       <div class="swiper-slide">
         <article class="work__card">
           <a href="${project.link}" class="work__link">
-            <img src="${project.image}" alt="Home Work Image" class="work__img" />
+            <img src="${project.images[0]}" alt="Home Work Image" class="work__img" />
             <i class="ri-arrow-right-circle-line work__icon"></i>
           </a>
           <h2 class="work__title">${project.title}</h2>
